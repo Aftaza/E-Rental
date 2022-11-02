2b0d87b001b2d742e64f85263fea9deb418c331f
Aftaza <115518264+Aftaza@users.noreply.github.com>
2022-11-02 14:00:39 +0700

    Add files via upload

---
+++ data.h
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <bits/stdc++.h>
+using namespace std;
+
+//ambil waktu 
+time_t sec = time(0);
+string tim = ctime(&sec);
+
+//variabel input
+int inpTipe, merek;
+int pay;
+char confirm;
+ map<string, string> client{ 
+ 		{ "name", "" },
+   		{ "address", "" }, 
+   		{ "telp", "" }
+   };
+   map<string, string>::iterator i;
+
+map<string, string> order{ 
+    { "merek", "" },
+      { "tglPeminjaman", "" }, 
+      { "tglKembali", "" },
+      { "tenggat", ""},
+      { "biaya", ""}
+   };
+   map<string, string>::iterator j;
+
+
+//variabel data
+   int selisih;
+   int Sum;
+   int change;
+   string price;
+   string tipe;
+string katalogMotor[] = {
+  "Yamaha X-Ride 125",
+  "Honda BeAT FI",
+  "Honda Scoopy FI",
+  "Honda Vario 150 eSP",
+  "Yamaha Mio J   "
+};
+string katalogMobil[] = {
+  "Mercedez benz", 
+  "Daihatsu Xenia",
+  "Honda All New City",
+  "Toyota Avanza", 
+  "Daihatsu Sirion", 
+};
+int hargaMobilPerHari[] = {
+  800000,
+  200000,
+  500000,
+  300000,
+  600000
+};
+int hargaMotorPerHari[] = {
+  200000,
+  100000,
+  150000,
+  300000,
+  100000
+};
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "Rental.cpp"
+
+int main()
+{
+  
+  console();
+    
+}
+
+void restart(){
+  stringstream ss;
+   stringstream st;
+  delay(5, 3);
+  client["name"].clear();
+  ss.str("");
+   st.str("");
+  main();
+}
